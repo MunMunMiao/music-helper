8ca543af72ec00340a9a45d1140a36f5cf1ef6d4
MunMun Miao <18216142+MunMunMiao@users.noreply.github.com>
2023-11-27 13:20:04 +0800

    no message

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'standalone',
+	webpack: (config, { isServer }) => {
+		config.externals = {
+			'@sparticuz/chromium-min': '@sparticuz/chromium-min',
+			'puppeteer-core': 'puppeteer-core'
+		}
+
+		return config
+	}
 }
 
 module.exports = nextConfig
